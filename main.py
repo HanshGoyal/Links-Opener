5f13d40ed1bd3ba46f7eee01ffb626f42cdcecc7
Hansh Goyal <99343982+HanshGoyal@users.noreply.github.com>
2023-06-23 19:20:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import webbrowser as wb
+import time
 with open('links.txt', 'r') as f:
     for line in f:
         a=line.strip()
         wb.open(a)
+        time.sleep(2) #cooldown time in seconds, change as per requirement
+        
